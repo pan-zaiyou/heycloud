ef20aee0ef385abe78bb88a4bc43dd05bbc637a9
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-05-30 00:30:12 +0800

    Update reset_password.ts

--- src/model/reset_password.ts
+++ src/model/reset_password.ts
@@ -2,9 +2,4 @@ export type ResetPasswordPayload = {
   email: string;
   password: string;
   email_code: string;
-  @if (session('status'))
-    <div class="alert alert-success">
-        {{ __('Your password has been successfully reset! Please log in with your new password.') }}
-    </div>
-@endif
 };
