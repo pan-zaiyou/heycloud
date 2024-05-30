5cf8f2e057a46b42071b2f8fae59f2e6e3b4ab6d
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-05-30 21:49:36 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -37,6 +37,7 @@ const config: DefaultConfigProps = {
     measurementId: getGA()
   },
   emojiEndpoint: "https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/{{code}}.png",
+  startYear: 2019
 };
 
 export default config;
