e1a0ffd2bcc40f7b2e65056e7f320d0c82393267
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-04 00:40:39 +0800

    Update LogoMain.tsx

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -1,38 +1,16 @@
+import React from 'react';
 import { useTheme } from '@mui/material/styles';
-// import logoDark from 'assets/images/logo-dark.png';
-// import logo from 'assets/images/logo.png';
-
-/**
- * if you want to use image instead of <svg> uncomment following.
- *
- * import logoDark from 'assets/images/logo-dark.png';
- * import logo from 'assets/images/logo.png';
- *
- */
-
-// ==============================|| LOGO SVG ||============================== //
 
 const LogoMain = ({ reverse, ...others }: { reverse?: boolean }) => {
   const theme = useTheme();
+
+  const logoSrc =
+    theme.palette.mode === 'dark'
+      ? '/src/assets/images/logo-dark.png'
+      : '/src/assets/images/logo.png';
+
   return (
-    /**
-     * if you want to use image instead of svg uncomment following, and comment out <svg> element.
-     *
-     * <img src={theme.palette.mode === 'dark' ? logoDark : logo} alt="Mantis" width="100" />
-     *
-     */
-    <>
-      <img
-        src={
-          theme.palette.mode === 'dark'
-            ? 'src/assets/images/logo-dark.png'
-            : 'src/assets/images/logo.png'
-        }
-        alt="HEYCLOUD"
-        width="150"
-        {...others}
-      />
-    </>
+    <img src={logoSrc} alt="MOEU" width="100" {...others} />
   );
 };
 
