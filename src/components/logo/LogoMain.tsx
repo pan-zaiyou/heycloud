8860df80f81935dc1ebcff776c98483d56d6b96a
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-03 23:45:46 +0800

    Update LogoMain.tsx

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -1,12 +1,12 @@
 import { useTheme } from '@mui/material/styles';
-// import logoDark from 'assets/images/logo-dark.svg';
-// import logo from 'assets/images/logo.svg';
+// import logoDark from 'assets/images/logo-dark.png';
+// import logo from 'assets/images/logo.png';
 
 /**
  * if you want to use image instead of <svg> uncomment following.
  *
- * import logoDark from 'assets/images/logo-dark.svg';
- * import logo from 'assets/images/logo.svg';
+ * import logoDark from 'assets/images/logo-dark.png';
+ * import logo from 'assets/images/logo.png';
  *
  */
 
@@ -28,8 +28,8 @@ const LogoMain = ({ reverse, ...others }: { reverse?: boolean }) => {
             ? 'src/assets/images/logo-dark.png'
             : 'src/assets/images/logo.png'
         }
-        alt="MOEU"
-        width="100"
+        alt="HEYCLOUD"
+        width="150"
         {...others}
       />
     </>
