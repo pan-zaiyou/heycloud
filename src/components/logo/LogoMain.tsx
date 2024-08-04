2f7870e7d9a99b4a73fcf13123af7f28ea0f0f68
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-04 12:44:09 +0800

    Update LogoMain.tsx

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -20,7 +20,7 @@ const LogoMain = ({ reverse, ...others }: { reverse?: boolean }) => {
      *
      */
     <>
-      <svg fill="none" width="118px" height="40px" viewBox="0 0 118 35" xmlns="http://www.w3.org/2000/svg">
+      <svg fill="none" width="50px" height="50px" viewBox="0 0 118 35" xmlns="http://www.w3.org/2000/svg">
         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
         <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
         <g id="SVGRepo_iconCarrier">
