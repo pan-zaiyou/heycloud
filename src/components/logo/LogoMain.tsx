7b65acd395c06ec8d0d7f200bb20736ded57be59
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-05 23:00:30 +0800

    Update LogoMain.tsx

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -19,10 +19,10 @@ const LogoMain = ({ reverse, ...others }: { reverse?: boolean }) => {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        width="250"
+        width="150"
         zoomAndPan="magnify"
         viewBox="0 0 375 375"
-        height="250"
+        height="150"
         preserveAspectRatio="xMidYMid meet"
         version="1.0"
         {...others}
