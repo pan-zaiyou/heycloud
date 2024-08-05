dc64addc0bfc0fac7a9b94042880ea82aa64c852
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-05 23:51:11 +0800

    Update LogoMain.tsx

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -21,7 +21,7 @@ const LogoMain = ({ reverse, ...others }: { reverse?: boolean }) => {
         xmlnsXlink="http://www.w3.org/1999/xlink"
         width="120"
         zoomAndPan="magnify"
-        viewBox="0 -50 300 375"
+        viewBox="0 -50 375 375"
         height="120"
         preserveAspectRatio="xMidYMid meet"
         version="1.0"
