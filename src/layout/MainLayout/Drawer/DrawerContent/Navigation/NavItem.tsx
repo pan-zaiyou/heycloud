99afdb86c7ef4cc3f270daa03641addc93f73eed
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-08 13:19:32 +0800

    Update NavItem.tsx

--- src/layout/MainLayout/Drawer/DrawerContent/Navigation/NavItem.tsx
+++ src/layout/MainLayout/Drawer/DrawerContent/Navigation/NavItem.tsx
@@ -1,17 +1,17 @@
-import React, { forwardRef, useEffect, ForwardRefExoticComponent, RefAttributes } from "react";
+import React, { forwardRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useTheme } from "@mui/material/styles";
-import { Avatar, Chip, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import { activeItem } from "@/store/reducers/menu";
-import { LinkTarget, NavItemType } from "@/types/menu";
+import { NavItemType } from "@/types/menu";
 import { RootStateProps } from "@/types/root";
 import { useTranslation } from "react-i18next";
 
 interface Props {
   item: NavItemType;
   level: number;
-  onClick?: () => void; // 添加 onClick 属性
+  onClick?: () => void;
 }
 
 const NavItem = ({ item, level, onClick }: Props) => {
@@ -21,131 +21,34 @@ const NavItem = ({ item, level, onClick }: Props) => {
   const menu = useSelector((state: RootStateProps) => state.menu);
   const { drawerOpen, openItem } = menu;
 
-  let itemTarget: LinkTarget = "_self";
-  if (item.target) {
-    itemTarget = "_blank";
-  }
-
-  let listItemProps: {
-    component: ForwardRefExoticComponent<RefAttributes<HTMLAnchorElement>> | string;
-    href?: string;
-    target?: LinkTarget;
-  } = { component: forwardRef((props, ref) => <Link {...props} ref={ref} to={item.url!} target={itemTarget} />) };
-  if (item?.external) {
-    listItemProps = { component: "a", href: item.url, target: itemTarget };
-  }
-
-  const Icon = item.icon!;
-  const itemIcon = item.icon ? <Icon style={{ fontSize: drawerOpen ? "1rem" : "1.25rem" }} /> : false;
-
-  const isSelected = openItem.findIndex((id) => id === item.id) > -1;
-
+  const isSelected = openItem.includes(item.id);
   const { pathname } = useLocation();
 
-  useEffect(() => {
-    if (pathname && pathname.includes("product-details")) {
-      if (item.url && item.url.includes("product-details")) {
-        dispatch(activeItem({ openItem: [item.id] }));
-      }
-    }
-
-    if (pathname && pathname.includes("kanban")) {
-      if (item.url && item.url.includes("kanban")) {
-        dispatch(activeItem({ openItem: [item.id] }));
-      }
-    }
-
+  React.useEffect(() => {
     if (pathname === item.url) {
       dispatch(activeItem({ openItem: [item.id] }));
     }
-    // eslint-disable-next-line
-  }, [pathname]);
-
-  const textColor = theme.palette.mode === "dark" ? "grey.400" : "text.primary";
-  const iconSelectedColor = theme.palette.mode === "dark" && drawerOpen ? "text.primary" : "primary.main";
+  }, [pathname, dispatch, item.url, item.id]);
 
   return (
     <ListItemButton
-      {...listItemProps}
+      component={item.external ? "a" : forwardRef((props, ref) => <Link {...props} ref={ref} to={item.url!} />)}
       disabled={item.disabled}
       selected={isSelected}
-      onClick={onClick} // 添加 onClick
-      sx={{
-        zIndex: 1201,
-        pl: drawerOpen ? `${level * 28}px` : 1.5,
-        py: !drawerOpen && level === 1 ? 1.25 : 1,
-        ...(drawerOpen && {
-          "&:hover": {
-            bgcolor: theme.palette.mode === "dark" ? "divider" : "primary.lighter"
-          },
-          "&.Mui-selected": {
-            bgcolor: theme.palette.mode === "dark" ? "divider" : "primary.lighter",
-            borderRight: `2px solid ${theme.palette.primary.main}`,
-            color: iconSelectedColor,
-            "&:hover": {
-              color: iconSelectedColor,
-              bgcolor: theme.palette.mode === "dark" ? "divider" : "primary.lighter"
-            }
-          }
-        }),
-        ...(!drawerOpen && {
-          "&:hover": {
-            bgcolor: "transparent"
-          },
-          "&.Mui-selected": {
-            "&:hover": {
-              bgcolor: "transparent"
-            },
-            bgcolor: "transparent"
-          }
-        })
-      }}
+      onClick={onClick}
     >
-      {itemIcon && (
-        <ListItemIcon
-          sx={{
-            minWidth: 28,
-            color: isSelected ? iconSelectedColor : textColor,
-            ...(!drawerOpen && {
-              borderRadius: 1.5,
-              width: 36,
-              height: 36,
-              alignItems: "center",
-              justifyContent: "center",
-              "&:hover": {
-                bgcolor: theme.palette.mode === "dark" ? "secondary.light" : "secondary.lighter"
-              }
-            }),
-            ...(!drawerOpen &&
-              isSelected && {
-                bgcolor: theme.palette.mode === "dark" ? "primary.900" : "primary.lighter",
-                "&:hover": {
-                  bgcolor: theme.palette.mode === "dark" ? "primary.darker" : "primary.lighter"
-                }
-              })
-          }}
-        >
-          {itemIcon}
+      {item.icon && (
+        <ListItemIcon>
+          <item.icon />
         </ListItemIcon>
       )}
-      {(drawerOpen || (!drawerOpen && level !== 1)) && (
-        <ListItemText
-          primary={
-            <Typography variant="h6" sx={{ color: isSelected ? iconSelectedColor : textColor }}>
-              {item.title && t(item.title, { ns: "title" })}
-            </Typography>
-          }
-        />
-      )}
-      {(drawerOpen || (!drawerOpen && level !== 1)) && item.chip && (
-        <Chip
-          color={item.chip.color}
-          variant={item.chip.variant}
-          size={item.chip.size}
-          label={item.chip.label}
-          avatar={item.chip.avatar && <Avatar>{item.chip.avatar}</Avatar>}
-        />
-      )}
+      <ListItemText
+        primary={
+          <Typography variant="h6">
+            {t(item.title)}
+          </Typography>
+        }
+      />
     </ListItemButton>
   );
 };
