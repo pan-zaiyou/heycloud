23612cab459741a63d4f45455fd7ce6095d6fd8a
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-08 17:14:37 +0800

    Update Avatar.tsx

--- src/components/@extended/Avatar.tsx
+++ src/components/@extended/Avatar.tsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react";
+import { ReactNode } from "react";
+
+// material-ui
 import { styled, useTheme, Theme } from "@mui/material/styles";
 import MuiAvatar from "@mui/material/Avatar";
 import { AvatarProps } from "@mui/material";
+
+// project import
 import getColors from "@/utils/getColors";
+
+// types
 import { AvatarTypeProps, ColorProps, ExtendedStyleProps, SizeProps } from "@/types/extended";
 
-// 导入头像路径
-import avatar1 from "@/assets/images/users/avatar-1.png";
-import avatar2 from "@/assets/images/users/avatar-2.png";
-import avatar3 from "@/assets/images/users/avatar-3.png";
-import avatar4 from "@/assets/images/users/avatar-4.png";
-import avatar5 from "@/assets/images/users/avatar-5.png";
+// ==============================|| AVATAR - COLOR STYLE ||============================== //
 
 interface AvatarStyleProps extends ExtendedStyleProps {
   variant?: AvatarProps["variant"];
@@ -25,29 +26,31 @@ function getColorStyle({ variant, theme, color, type }: AvatarStyleProps) {
     case "filled":
       return {
         color: contrastText,
-        backgroundColor: main,
+        backgroundColor: main
       };
     case "outlined":
       return {
         color: main,
         border: "1px solid",
         borderColor: main,
-        backgroundColor: "transparent",
+        backgroundColor: "transparent"
       };
     case "combined":
       return {
         color: main,
         border: "1px solid",
         borderColor: light,
-        backgroundColor: lighter,
+        backgroundColor: lighter
       };
     default:
       return {
         color: main,
-        backgroundColor: lighter,
+        backgroundColor: lighter
       };
   }
 }
+
+// ==============================|| AVATAR - SIZE STYLE ||============================== //
 
 function getSizeStyle(size?: SizeProps) {
   switch (size) {
@@ -56,41 +59,43 @@ function getSizeStyle(size?: SizeProps) {
         border: "2px solid",
         fontSize: "0.675rem",
         width: 20,
-        height: 20,
+        height: 20
       };
     case "xs":
       return {
         fontSize: "0.75rem",
         width: 24,
-        height: 24,
+        height: 24
       };
     case "sm":
       return {
         fontSize: "0.875rem",
         width: 32,
-        height: 32,
+        height: 32
       };
     case "lg":
       return {
         fontSize: "1.2rem",
         width: 52,
-        height: 52,
+        height: 52
       };
     case "xl":
       return {
         fontSize: "1.5rem",
         width: 64,
-        height: 64,
+        height: 64
       };
     case "md":
     default:
       return {
         fontSize: "1rem",
         width: 40,
-        height: 40,
+        height: 40
       };
   }
 }
+
+// ==============================|| STYLED - AVATAR ||============================== //
 
 interface StyleProps {
   color: ColorProps;
@@ -101,60 +106,37 @@ interface StyleProps {
 }
 
 const AvatarStyle = styled(MuiAvatar, {
-  shouldForwardProp: (prop) => prop !== "color" && prop !== "type" && prop !== "size",
+  shouldForwardProp: (prop) => prop !== "color" && prop !== "type" && prop !== "size"
 })(({ theme, variant, color, type, size }: StyleProps) => ({
   ...getSizeStyle(size),
   ...getColorStyle({ variant, theme, color, type }),
   ...(size === "badge" && {
-    borderColor: theme.palette.background.default,
-  }),
+    borderColor: theme.palette.background.default
+  })
 }));
 
-const avatars = [
-  avatar1,
-  avatar2,
-  avatar3,
-  avatar4,
-  avatar5,
-];
+// ==============================|| EXTENDED - AVATAR ||============================== //
 
 export interface Props extends AvatarProps {
   color?: ColorProps;
+  children?: ReactNode | string;
   type?: AvatarTypeProps;
   size?: SizeProps;
-  rotateAvatars?: boolean;  // 控制是否轮询头像
 }
 
 export default function Avatar({
   variant = "circular",
+  children,
   color = "primary",
   type,
   size = "md",
-  rotateAvatars = true, // 默认值为 false
   ...others
 }: Props) {
   const theme = useTheme();
-  const [currentAvatarIndex, setCurrentAvatarIndex] = useState(0);
-
-  useEffect(() => {
-    if (rotateAvatars) {
-      const interval = setInterval(() => {
-        setCurrentAvatarIndex((prevIndex) => (prevIndex + 1) % avatars.length);
-      }, 3000); // 每3秒切换一次头像
-
-      return () => clearInterval(interval); // 清除定时器
-    }
-  }, [rotateAvatars]);
 
   return (
-    <AvatarStyle
-      variant={variant}
-      theme={theme}
-      color={color}
-      type={type}
-      size={size}
-      src={avatars[currentAvatarIndex]} // 使用轮询头像路径
-      {...others}
-    />
+    <AvatarStyle variant={variant} theme={theme} color={color} type={type} size={size} {...others}>
+      {children}
+    </AvatarStyle>
   );
 }
