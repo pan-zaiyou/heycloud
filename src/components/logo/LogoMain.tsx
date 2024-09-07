c2ee2b4da0fedc66101c6e3191a66b9bbde35832
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-09-07 14:43:13 +0800

    Update LogoMain.tsx

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -15,28 +15,16 @@ import { useTheme } from '@mui/material/styles';
 
 const LogoMain = ({ reverse, ...others }: { reverse?: boolean }) => {
   const theme = useTheme();
-  const isDarkMode = theme.palette.mode === 'dark';
-
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <img
-        src={isDarkMode ? "暗黑模式logourl" : "亮色模式logourl"}
-        alt="logo"
-        width="100"
-        {...others}
-      />
-      <span
-        style={{
-          fontSize: '20px', // 调整为你想要的字体大小
-          fontWeight: 'bold', // 加粗
-          marginLeft: '5px', // 根据需要调整间距
-          color: isDarkMode ? '#fff' : '#000', // 暗黑模式下为白色，其他模式下为黑色
-          fontFamily: '黑体, sans-serif', // 设置字体为黑体
-        }}
-      >
-        跨越长城
-      </span>
-    </div>
+    /**
+     * if you want to use image instead of svg uncomment following, and comment out <svg> element.
+     *
+     * <img src={theme.palette.mode === 'dark' ? logoDark : logo} alt="Mantis" width="100" />
+     *
+     */
+    <>
+      <img src={theme.palette.mode === 'dark' ? "暗黑模式logourl" : "亮色模式logourl"} alt="MOEU" width="100" />
+    </>
   );
 };
 
